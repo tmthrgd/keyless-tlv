5d529de6fd454dd14920b2cc69a80315d5efb4f7
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-03-06 16:21:32 +1030

    Initial upload

---
+++ kssl.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+
+	"github.com/cloudflare/gokeyless"
+	gkserver "github.com/cloudflare/gokeyless/server"
+)
+
+const OpRSADecryptRaw gokeyless.Op = 0x08
+
+func encodeOperation(id uint32, operation *gokeyless.Operation) ([]byte, error) {
+	h := gokeyless.NewHeader(operation)
+	h.ID = id
+	return h.MarshalBinary()
+}
+
+func encodeResponse(id uint32, payload []byte) ([]byte, error) {
+	return encodeOperation(id, &gokeyless.Operation{
+		Opcode:  gokeyless.OpResponse,
+		Payload: payload,
+	})
+}
+
+func encodeError(id uint32, err gokeyless.Error) ([]byte, error) {
+	return encodeOperation(id, &gokeyless.Operation{
+		Opcode:  gokeyless.OpError,
+		Payload: []byte{byte(err)},
+	})
+}
+
+type server struct {
+	Keys gkserver.Keystore
+	gsrv gkserver.Server
+}
+
+func newServer(keys gkserver.Keystore) server {
+	return server{
+		Keys: keys,
+		gsrv: gkserver.Server{Keys: keys},
+	}
+}
+
+func (s server) LoadKeysFromDir(dir string, LoadKey func([]byte) (crypto.Signer, error)) error {
+	return s.gsrv.LoadKeysFromDir(dir, LoadKey)
+}
+
+func (s server) Handle(buf []byte) (out []byte, err error) {
+	h := new(gokeyless.Header)
+
+	if err = h.UnmarshalBinary(buf); err != nil {
+		return
+	}
+
+	if 8+int(h.Length) > len(buf) {
+		log.Printf("%s: Header length is %dB beyond end of buffer", gokeyless.ErrFormat, 8+int(h.Length)-len(buf))
+
+		return encodeError(h.ID, gokeyless.ErrFormat)
+	}
+
+	h.Body = new(gokeyless.Operation)
+
+	if err = h.Body.UnmarshalBinary(buf[8 : 8+h.Length]); err != nil {
+		log.Println(err)
+
+		return encodeError(h.ID, gokeyless.ErrFormat)
+	}
+
+	log.Printf("version:%d.%d id:%d body:%s", h.MajorVers, h.MinorVers, h.ID, h.Body)
+
+	var opts crypto.SignerOpts
+	var key crypto.Signer
+	var ok bool
+
+	switch h.Body.Opcode {
+	case gokeyless.OpPing:
+		return encodeOperation(h.ID, &gokeyless.Operation{
+			Opcode:  gokeyless.OpPong,
+			Payload: h.Body.Payload,
+		})
+	case gokeyless.OpRSADecrypt, OpRSADecryptRaw:
+		if key, ok = s.Keys.Get(h.Body); !ok {
+			return encodeError(h.ID, gokeyless.ErrKeyNotFound)
+		}
+
+		if _, ok = key.Public().(*rsa.PublicKey); !ok {
+			log.Printf("%s: Key is not RSA\n", gokeyless.ErrCrypto)
+
+			return encodeError(h.ID, gokeyless.ErrCrypto)
+		}
+
+		var ptxt []byte
+
+		if h.Body.Opcode == OpRSADecryptRaw {
+			rsaKey, ok := key.(*rsa.PrivateKey)
+			if !ok {
+				log.Printf("%s: Key is not rsa.PrivateKey\n", gokeyless.ErrCrypto)
+
+				return encodeError(h.ID, gokeyless.ErrCrypto)
+			}
+
+			ptxt, err = rsaRawDecrypt(rand.Reader, rsaKey, h.Body.Payload)
+		} else {
+			rsaKey, ok := key.(crypto.Decrypter)
+			if !ok {
+				log.Printf("%s: Key is not Decrypter\n", gokeyless.ErrCrypto)
+
+				return encodeError(h.ID, gokeyless.ErrCrypto)
+			}
+
+			ptxt, err = rsaKey.Decrypt(rand.Reader, h.Body.Payload, nil)
+		}
+
+		if err != nil {
+			log.Printf("%s: Decryption error: %v", gokeyless.ErrCrypto, err)
+
+			return encodeError(h.ID, gokeyless.ErrCrypto)
+		}
+
+		return encodeResponse(h.ID, ptxt)
+	case gokeyless.OpRSASignMD5SHA1, gokeyless.OpECDSASignMD5SHA1:
+		opts = crypto.MD5SHA1
+	case gokeyless.OpRSASignSHA1, gokeyless.OpECDSASignSHA1:
+		opts = crypto.SHA1
+	case gokeyless.OpRSASignSHA224, gokeyless.OpECDSASignSHA224:
+		opts = crypto.SHA224
+	case gokeyless.OpRSASignSHA256, gokeyless.OpECDSASignSHA256:
+		opts = crypto.SHA256
+	case gokeyless.OpRSASignSHA384, gokeyless.OpECDSASignSHA384:
+		opts = crypto.SHA384
+	case gokeyless.OpRSASignSHA512, gokeyless.OpECDSASignSHA512:
+		opts = crypto.SHA512
+	//case gokeyless.OpActivate:
+	case gokeyless.OpPong, gokeyless.OpResponse, gokeyless.OpError:
+		log.Printf("%s: %s is not a valid request Opcode\n", gokeyless.ErrUnexpectedOpcode, h.Body.Opcode)
+
+		return encodeError(h.ID, gokeyless.ErrUnexpectedOpcode)
+	default:
+		return encodeError(h.ID, gokeyless.ErrBadOpcode)
+	}
+
+	if key, ok = s.Keys.Get(h.Body); !ok {
+		log.Println(gokeyless.ErrKeyNotFound)
+
+		return encodeError(h.ID, gokeyless.ErrKeyNotFound)
+	}
+
+	// Ensure we don't perform an ECDSA sign for an RSA request.
+	switch h.Body.Opcode {
+	case gokeyless.OpRSASignMD5SHA1,
+		gokeyless.OpRSASignSHA1,
+		gokeyless.OpRSASignSHA224,
+		gokeyless.OpRSASignSHA256,
+		gokeyless.OpRSASignSHA384,
+		gokeyless.OpRSASignSHA512:
+		if _, ok := key.Public().(*rsa.PublicKey); !ok {
+			log.Printf("%s: request is RSA, but key isn't\n", gokeyless.ErrCrypto)
+
+			return encodeError(h.ID, gokeyless.ErrCrypto)
+		}
+	}
+
+	sig, err := key.Sign(rand.Reader, h.Body.Payload, opts)
+
+	if err != nil {
+		log.Printf("%s: Signing error: %v\n", gokeyless.ErrCrypto, err)
+
+		return encodeError(h.ID, gokeyless.ErrCrypto)
+	}
+
+	return encodeResponse(h.ID, sig)
+}
---
+++ rsa_raw.go
@@ -0,0 +1,148 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Taken from src/crypto/rsa/rsa.go
+
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"io"
+	"math/big"
+)
+
+var bigZero = big.NewInt(0)
+var bigOne = big.NewInt(1)
+
+// modInverse returns ia, the inverse of a in the multiplicative group of prime
+// order n. It requires that a be a member of the group (i.e. less than n).
+func modInverse(a, n *big.Int) (ia *big.Int, ok bool) {
+	g := new(big.Int)
+	x := new(big.Int)
+	y := new(big.Int)
+	g.GCD(x, y, a, n)
+	if g.Cmp(bigOne) != 0 {
+		// In this case, a and n aren't coprime and we cannot calculate
+		// the inverse. This happens because the values of n are nearly
+		// prime (being the product of two primes) rather than truly
+		// prime.
+		return
+	}
+
+	if x.Cmp(bigOne) < 0 {
+		// 0 is not the multiplicative inverse of any element so, if x
+		// < 1, then x is negative.
+		x.Add(x, n)
+	}
+
+	return x, true
+}
+
+// decrypt performs an RSA decryption, resulting in a plaintext integer. If a
+// random source is given, RSA blinding is used.
+func rsaDecrypt(random io.Reader, priv *rsa.PrivateKey, c *big.Int) (m *big.Int, err error) {
+	// TODO(agl): can we get away with reusing blinds?
+	if c.Cmp(priv.N) > 0 {
+		err = rsa.ErrDecryption
+		return
+	}
+
+	var ir *big.Int
+	if random != nil {
+		// Blinding enabled. Blinding involves multiplying c by r^e.
+		// Then the decryption operation performs (m^e * r^e)^d mod n
+		// which equals mr mod n. The factor of r can then be removed
+		// by multiplying by the multiplicative inverse of r.
+
+		var r *big.Int
+
+		for {
+			r, err = rand.Int(random, priv.N)
+			if err != nil {
+				return
+			}
+			if r.Cmp(bigZero) == 0 {
+				r = bigOne
+			}
+			var ok bool
+			ir, ok = modInverse(r, priv.N)
+			if ok {
+				break
+			}
+		}
+		bigE := big.NewInt(int64(priv.E))
+		rpowe := new(big.Int).Exp(r, bigE, priv.N)
+		cCopy := new(big.Int).Set(c)
+		cCopy.Mul(cCopy, rpowe)
+		cCopy.Mod(cCopy, priv.N)
+		c = cCopy
+	}
+
+	if priv.Precomputed.Dp == nil {
+		m = new(big.Int).Exp(c, priv.D, priv.N)
+	} else {
+		// We have the precalculated values needed for the CRT.
+		m = new(big.Int).Exp(c, priv.Precomputed.Dp, priv.Primes[0])
+		m2 := new(big.Int).Exp(c, priv.Precomputed.Dq, priv.Primes[1])
+		m.Sub(m, m2)
+		if m.Sign() < 0 {
+			m.Add(m, priv.Primes[0])
+		}
+		m.Mul(m, priv.Precomputed.Qinv)
+		m.Mod(m, priv.Primes[0])
+		m.Mul(m, priv.Primes[1])
+		m.Add(m, m2)
+
+		for i, values := range priv.Precomputed.CRTValues {
+			prime := priv.Primes[2+i]
+			m2.Exp(c, values.Exp, prime)
+			m2.Sub(m2, m)
+			m2.Mul(m2, values.Coeff)
+			m2.Mod(m2, prime)
+			if m2.Sign() < 0 {
+				m2.Add(m2, prime)
+			}
+			m2.Mul(m2, values.R)
+			m.Add(m, m2)
+		}
+	}
+
+	if ir != nil {
+		// Unblind.
+		m.Mul(m, ir)
+		m.Mod(m, priv.N)
+	}
+
+	return
+}
+
+func rsaRawDecrypt(random io.Reader, priv *rsa.PrivateKey, ciphertext []byte) (plaintext []byte, err error) {
+	k := (priv.N.BitLen() + 7) / 8
+	if k < 11 {
+		err = rsa.ErrDecryption
+		return
+	}
+
+	c := new(big.Int).SetBytes(ciphertext)
+	m, err := rsaDecrypt(random, priv, c)
+
+	if err != nil {
+		return
+	}
+
+	return leftPad(m.Bytes(), k), nil
+}
+
+// leftPad returns a new slice of length size. The contents of input are right
+// aligned in the new slice.
+func leftPad(input []byte, size int) (out []byte) {
+	n := len(input)
+	if n > size {
+		n = size
+	}
+	out = make([]byte, size)
+	copy(out[len(out)-n:], input)
+	return
+}
