603e1fe043bc5aba9c1bfa2837a66d0afcf0f598
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-12-26 17:51:01 +1030

    Style change in (*RequestHandler).ReadKeyFile

--- key-file.go
+++ key-file.go
@@ -19,11 +19,9 @@ func (h *RequestHandler) ReadKeyFile(path string) error {
 	}
 
 	h.Lock()
-
 	h.PrivateKey = keyfile[:ed25519.PrivateKeySize]
 	h.PublicKey = h.PrivateKey.Public().(ed25519.PublicKey)
 	h.Authorisation = keyfile[ed25519.PrivateKeySize:]
-
 	h.Unlock()
 	return nil
 }
